b06903322353b006a4d2f02376e0d4eb9f7b96d1
douglas <douglashiura@gmail.com>
2021-10-16 09:42:21 -0300

    fix: configura acessorios

--- uniformidade-em-teste-de-unidade/src/main/java/br/ies/tcc/uniformidade/em/teste/de/unidade/Pessoa.java
+++ uniformidade-em-teste-de-unidade/src/main/java/br/ies/tcc/uniformidade/em/teste/de/unidade/Pessoa.java
@@ -1,5 +1,8 @@
 package br.ies.tcc.uniformidade.em.teste.de.unidade;
 
 public class Pessoa {
-
+public static void main(String[] args) {
+	
+	
+}
 }
--- uniformidade-em-teste-de-unidade/src/main/java/br/ies/tcc/uniformidade/em/teste/de/unidade/Veiculo.java
+++ uniformidade-em-teste-de-unidade/src/main/java/br/ies/tcc/uniformidade/em/teste/de/unidade/Veiculo.java
@@ -2,14 +2,12 @@ package br.ies.tcc.uniformidade.em.teste.de.unidade;
 
 public class Veiculo {
 
-
 	public Integer obterVelocidade() {
-		return null;
+		return 200;
 	}
 
 	public void fixaOMotorista(Pessoa pessoa) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 }
